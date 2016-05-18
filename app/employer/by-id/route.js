f1430b50352b0b7d9eaacac00b471003bd2b4ae5
Zach Garwood <zachgarwood@gmail.com>
2016-05-18 18:49:16 -0500

    Added lobbyist to employer template

--- app/employer/by-id/route.js
+++ app/employer/by-id/route.js
@@ -4,6 +4,10 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('employer', params.employer_id);
   },
+  setupController(controller, model) {
+    this._super(...arguments);
+    Ember.set(controller, 'lobbyists', this.store.query('lobbyist', { 'employer_id': model.get('id') }));
+  },
   renderTemplate() {
     this.render('employer.by-id', {
       into: 'application',
